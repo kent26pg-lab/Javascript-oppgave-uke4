e8a63eb2104fa183f7f6770e8708e6edf47412f5
kent <kent.26pg@gmail.com>
2026-05-27 13:42:16 +0200

    made a array of objects and begun to structure the html

---
+++ script.js
@@ -0,0 +1,32 @@
+const dcKarakterer = [
+    {
+        navn: "Batman",
+        bilde: "images/batman.jpg",
+        fakta: "Etter å ha mistet foreldrene sine, sverget Bruce Wayne å bekjempe kriminalitet i Gotham City. Han har ingen superkrefter, men bruker intelligens, kampsport og avansert teknologi."
+    },
+    {
+        navn: "Superman",
+        bilde: "images/superman.jpg",
+        fakta: "Sendt til jorden fra den døende planeten Krypton. Under jordens gule sol har han superkrefter som usårbarhet, evnen til å fly og røntgensyn, men han svekkes av kryptonitt."
+    },
+    {
+        navn: "Wonder Woman",
+        bilde: "images/wonderwoman.jpg",
+        fakta: "Prinsesse Diana av Themyscira er en amasonekriger med krefter fra de greske gudene. Hun kjemper for fred og rettferdighet med sitt magiske sannhetslasso."
+    },
+    {
+        navn: "The Joker",
+        bilde: "images/joker.jpg",
+        fakta: "Gothams mest beryktede kriminelle og Batmans erkefiende. Han er en uforutsigbar, nihilistisk kaosbaktanke som gjemmer sine onde planer bak et klovneansikt."
+    },
+    {
+        navn: "Lex Luthor",
+        bilde: "images/lexluthor.jpg",
+        fakta: "En av verdens smarteste menn og en milliardær-forretningsmann. Han ser på Superman som en trussel mot menneskeheten og bruker sin enorme formue og intellekt på å bekjempe ham."
+    },
+    {
+        navn: "Harley Quinn",
+        bilde: "images/harleyquinn.jpg",
+        fakta: "Tidligere psykiater ved Arkham Asylum under navnet Harleen Quinzel. Hun ble fascinert av Jokeren, hjalp ham å rømme, og ble hans akrobatiske og uforutsigbare partner i partner-in-crime."
+    }
+];
