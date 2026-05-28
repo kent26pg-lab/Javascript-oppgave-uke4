cb3d6309ae6acf7810cced43733518c2651c0fe7
kent <kent.26pg@gmail.com>
2026-05-28 12:36:34 +0200

    added paths for the images. they wouldnt show up in github pages

--- script.js
+++ script.js
@@ -3,32 +3,32 @@
 const dcCharacters = [
     {
         name: "Batman",
-        image: "images/batman.jpg",
+        image: "/images/batman.jpg",
         facts: "Etter å ha mistet foreldrene sine, sverget Bruce Wayne å bekjempe kriminalitet i Gotham City. Han har ingen superkrefter, men bruker intelligens, kampsport og avansert teknologi."
     },
     {
         name: "Superman",
-        image: "images/superman.jpg",
+        image: "/images/superman.jpg",
         facts: "Sendt til jorden fra den døende planeten Krypton. Under jordens gule sol har han superkrefter som usårbarhet, evnen til å fly og røntgensyn, men han svekkes av kryptonitt."
     },
     {
         name: "Wonder Woman",
-        image: "images/wonderwoman.jpg",
+        image: "/images/wonderwoman.jpg",
         facts: "Prinsesse Diana av Themyscira er en amasonekriger med krefter fra de greske gudene. Hun kjemper for fred og rettferdighet med sitt magiske sannhetslasso."
     },
     {
         name: "The Joker",
-        image: "images/joker.jpg",
+        image: "/images/joker.jpg",
         facts: "Gothams mest beryktede kriminelle og Batmans erkefiende. Han er en uforutsigbar, nihilistisk kaosbaktanke som gjemmer sine onde planer bak et klovneansikt."
     },
     {
         name: "Lex Luthor",
-        image: "images/lexluthor.jpg",
+        image: "/images/lexluthor.jpg",
         facts: "En av verdens smarteste menn og en milliardær-forretningsmann. Han ser på Superman som en trussel mot menneskeheten og bruker sin enorme formue og intellekt på å bekjempe ham."
     },
     {
         name: "Harley Quinn",
-        image: "images/harleyquinn.jpg",
+        image: "/images/harleyquinn.jpg",
         facts: "Tidligere psykiater ved Arkham Asylum under navnet Harleen Quinzel. Hun ble fascinert av Jokeren, hjalp ham å rømme, og ble hans akrobatiske og uforutsigbare partner-in-crime."
     }
 ];
